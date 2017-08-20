cb00c343ea9d0f4b91143462ae05f03392bf5eaf
santi <santi@DESKTOP-EDP51L7>
2017-08-20 16:08:49 -0600

    ajskñjfdañkdsñl

--- Prueba/Prueba/Global.asax.cs
+++ Prueba/Prueba/Global.asax.cs
@@ -13,7 +13,7 @@ namespace Prueba
     {
         void Application_Start(object sender, EventArgs e)
         {
-            //khjdflkw
+            //khjdflkw.jhjk
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }

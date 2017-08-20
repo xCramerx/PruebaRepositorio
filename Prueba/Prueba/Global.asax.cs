f073c00552038194bae0f2666747e17b717c870c
santi <santi@DESKTOP-EDP51L7>
2017-08-20 16:03:11 -0600

    holalalla

--- Prueba/Prueba/Global.asax.cs
+++ Prueba/Prueba/Global.asax.cs
@@ -13,7 +13,7 @@ namespace Prueba
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }

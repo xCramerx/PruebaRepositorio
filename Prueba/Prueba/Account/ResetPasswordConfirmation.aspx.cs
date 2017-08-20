2681851b0fdeba3a046a07f15c1df0fd1b1ef070
santi <santi@DESKTOP-EDP51L7>
2017-08-20 16:00:17 -0600

    hola

---
+++ Prueba/Prueba/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Prueba.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Prueba/Prueba/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prueba.Startup))]
+namespace Prueba
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
